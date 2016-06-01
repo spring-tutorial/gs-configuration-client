dc53764f529dd7591f413faed508bb6516b9356c
damiankam <demon.kaminsky@gmail.com>
2016-06-01 19:31:48 +0300

    done

---
+++ src/main/java/hello/ConfigClientApplication.java
@@ -0,0 +1,32 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by dima on 6/1/16.
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
+
+@RefreshScope
+@RestController
+class MessageRestController {
+
+    @Value("${message}")
+    private String message;
+
+    @RequestMapping("/message")
+    String getMessage() {
+        return this.message;
+    }
+}
